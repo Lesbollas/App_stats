29a1506471c4f212f294a0abb77f2883d0f7ce59
Lesbollas <leoarcey@gmail.com>
2023-03-27 17:51:08 +0200

    ignore pck

--- Env/Scripts/Main.py
+++ Env/Scripts/Main.py
@@ -11,23 +11,33 @@ print (f'this is average of league : {df_average}')
 
  
 
-fenetre = Tk()
 
 
 
-#Cliquer pour afficher 
-def afficher_variable():
-    var = df_average
-    resultat.config(text=var)
-
-label = Label(fenetre, text="Cliquez sur le bouton pour afficher la moyenne de la league. ")
-label.pack()
-resultat = Label(fenetre)
-resultat.pack()
-
-bouton = Button(fenetre, text="Afficher la variable", command=afficher_variable)
-bouton.pack()
 
 
-fenetre.mainloop()
-###  2322
+
+
+
+
+
+
+
+ 
+### Fenêtre graphique ###
+# fenetre = Tk()
+# #Cliquer pour afficher 
+# def afficher_variable():
+#     var = df_average
+#     resultat.config(text=var)
+
+# label = Label(fenetre, text="Cliquez sur le bouton pour afficher la moyenne de la league. ")
+# label.pack()
+# resultat = Label(fenetre)
+# resultat.pack()
+
+# bouton = Button(fenetre, text="Afficher la variable", command=afficher_variable)
+# bouton.pack()
+
+# fenetre.mainloop()
+#######################################
